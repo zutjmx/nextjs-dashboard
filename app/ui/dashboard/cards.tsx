fa1727ebece7ce4c8784945eb9d0f3f8d28e1189
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-12 12:38:00 -0600

    Update README with new course curriculum link, implement loading state for dashboard, and refactor dashboard components for improved data fetching and rendering

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,28 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+import { fetchCardData } from '@/app/lib/data';
+
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
