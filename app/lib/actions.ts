6d08464791fbaba5832376ced6ab8ee1d7e29926
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-18 12:54:25 -0600

    Se comenta la simulación de error en la acción de borrado

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -71,10 +71,8 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
-  
-  throw new Error('Failed to Delete Invoice'); //Para simular un error y probar el manejo de errores en el componente de error.tsx
-  
+export async function deleteInvoice(id: string) {  
+  //throw new Error('Failed to Delete Invoice'); //Para simular un error y probar el manejo de errores en el componente de error.tsx  
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
