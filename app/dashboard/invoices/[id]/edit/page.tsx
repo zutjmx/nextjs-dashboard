28f06060762268674c66bace0a19efd3a1fe02db
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-18 12:51:45 -0600

    Se agregan componentes para el manejo de errores en las acciones de creación, edición y borrado

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById,fetchCustomers } from '@/app/lib/data';
+
+import { notFound } from 'next/navigation';
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -9,6 +11,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,10 +29,17 @@ export async function createInvoice(formData: FormData) {
     // Test it out:
     console.log({ customerId, amount, status, amountInCents, date });
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+      await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;  
+    } catch (error) {
+      console.error('Error creating invoice:', error);
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -49,17 +56,30 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
- 
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;       
+  } catch (error) {
+    console.error('Error updating invoice:', error);
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  
+  throw new Error('Failed to Delete Invoice'); //Para simular un error y probar el manejo de errores en el componente de error.tsx
+  
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    console.error('Error deleting invoice:', error);
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
