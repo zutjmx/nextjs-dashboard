7e8b48a40a81fa34394d0cd466e56065969e2610
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-19 11:54:07 -0600

    Se realiza deploy de prueba 19/03/2026 11:53

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,7 +72,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {  
-  //throw new Error('Failed to Delete Invoice'); //Para simular un error y probar el manejo de errores en el componente de error.tsx  
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
